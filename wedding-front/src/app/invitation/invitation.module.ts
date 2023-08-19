13d0d2f10cc793a73c97bdde63a8d37f2fae021a
cristian.mihaita@maxcode.net <cristian.mihaita@maxcode.net>
2023-08-19 17:02:54 +0300

    Add main components

--- wedding-front/src/app/invitation/invitation.module.ts
+++ wedding-front/src/app/invitation/invitation.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { InvitationRoutingModule } from './invitation-routing.module';
 import { InvitationPageComponent } from './invitation-page/invitation-page.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 
 @NgModule({
   declarations: [
-    InvitationPageComponent
+    InvitationPageComponent,
+    ToolbarComponent,
+    MainContentComponent
   ],
   imports: [
     CommonModule,
